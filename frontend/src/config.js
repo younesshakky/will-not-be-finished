71161fe652f9812acf1dde381ebeeeb8be153ff0
Youness <youness@theMatrix.theMatrix.io>
2018-04-30 13:31:57 +0100

    fix url suffix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,7 +3,7 @@ const config = {}
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname == 'projectly.surge.sh') {
-  config.api_url = 'http://projectly-api.herokuapp.com/'
+  config.api_url = 'http://projectly-api.herokuapp.com/api'
 } else {
   config.api_url = 'http://localhost:9000/api'
 }
